7a164de74c8ba4e3871fc473950fa9399fbcbbd2
Kuldeep Singh <83647746+kuldeep2040@users.noreply.github.com>
2025-08-06 18:52:04 +0530

    Update page.jsx

--- app/(main)/resume/page.jsx
+++ app/(main)/resume/page.jsx
@@ -5,7 +5,7 @@ export default async function ResumePage() {
   const resume = await getResume();
 
   return (
-    <div className="container mx-auto py-6">
+    <div className="container px-4 sm:px-6 md:px-8 lg:px-12 xl:px-16 py-6">
       <ResumeBuilder initialContent={resume?.content} />
     </div>
   );
